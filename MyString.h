ba88c024feb23d94f8381e16b652f9d6b82a821d
mohammed999alm <88013257+mohammed999alm@users.noreply.github.com>
2024-04-12 19:04:49 +0200

    Add project files.

---
+++ MyString.h
@@ -0,0 +1,319 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+class MyString
+{
+private:
+
+	string _value;
+
+
+	//'A' = 65 , a = 97 , 97 - 32 = 65.
+//In this equation we are able to convert any lower case letter to upper case letter
+//by subtracting 32 from it to get converted successfully.
+    static char toUpperCase(char letter)
+	{
+
+		if (letter >= 'a' && letter <= 'z')
+		{
+			letter = letter - 32;
+
+			return letter;
+		}
+
+		return letter;
+	}
+
+
+	//'A' = 65 , a = 97 , 65 - 32 = 97.
+	//In this equation we are able to convert any upper case letter to lower case letter
+	//by adding 32 to it to get converted successfully.
+    static char toLowerCase(char letter)
+	{
+		if (letter >= 'A' && letter <= 'Z')
+		{
+			letter = letter + 32;
+
+			return letter;
+		}
+
+		return letter;
+	}
+
+	static bool isUpperCase(char letter)
+	{
+		return (letter >= 'A' && letter <= 'Z');
+	}
+
+	static bool isLowerCase(char letter) 
+	{
+		return (letter >= 'a' && letter <= 'z');
+	}
+
+	static char invertCharacter(char letter)
+	{
+		return (isLowerCase(letter)) ? toUpperCase(letter) : toLowerCase(letter);
+	}
+
+
+	//Take Only c string style.
+	static int _customLength(const char* str)
+	{
+		int counter = 0;
+
+		while (*str != '\0')
+		{
+			counter++;
+			str++;
+		}
+
+		return counter;
+	}
+
+public:
+
+	MyString()
+	{
+		_value;
+	}
+
+	MyString(string value)
+	{
+		_value = value;
+	}
+
+
+	void setValue(string value)
+	{
+		_value = value;
+	}
+
+	string getValue()
+	{
+		return _value;
+	}
+
+	__declspec(property(get = getValue, put = setValue)) string value;
+
+
+	static short countWords(string text)
+	{
+		short pos = 0;
+		string word = "";
+		string delimiter = " ";
+
+		short wordCounter = 0;
+
+		while ((pos = text.find(delimiter)) != string::npos) 
+		{
+			word = text.substr(0, pos); 
+
+			if (word != "")
+				wordCounter++;
+
+			text.erase(0, pos + delimiter.length());
+		}
+
+		if (text != "")
+			wordCounter++;
+
+
+		return wordCounter;
+	}
+
+
+	short countWords()
+	{
+		return countWords(_value);
+	}
+
+
+
+	static string toUpperCase(string text) 
+	{
+		for (int i = 0; i < text.length(); i++) 
+		{
+			text[i] = toUpperCase(text[i]);
+		}
+
+		return text;
+	}
+
+	string toUpperCase() 
+	{
+		return toUpperCase(_value);
+	}
+
+	static string toLowerCase(string text)
+	{
+		for (int i = 0; i < text.length(); i++)
+		{
+			text[i] = toLowerCase(text[i]);
+		}
+
+		return text;
+	}
+
+	string toLowerCase()
+	{
+		return toLowerCase(_value);
+	}
+
+	static string invertString(string text) 
+	{
+		for (int i = 0; i < text.length(); i++)
+		{
+			text[i] = invertCharacter(text[i]);
+		}
+
+		return text;
+	}
+
+    string invertString()
+	{
+		return invertString(_value);
+	}
+
+	static string mySubstr(string text, short start, short end) 
+	{
+
+		string word = "";
+
+		short length = text.length();
+
+		if (start < 0 || start > length)
+			return "";
+
+		if (end > length)
+			end = length;
+
+
+		for (int i = start; i < end; i++)
+		{
+			word += text[i];
+		}
+
+		return word;
+	}
+
+	string mySubstr(short start, short end) 
+	{
+		return mySubstr(_value, start, end);
+	}
+
+
+	static int customLength(string text) 
+	{
+		const char* str = text.c_str();
+
+		return _customLength(str);
+	}
+
+	int customLength()
+	{
+		const char* str = _value.c_str();
+
+		return _customLength(str);
+	}
+
+	static string firstLetterWordUpperCase(string text) 
+	{
+		bool isFirstLetter = true;
+
+		short length = customLength(text);
+
+		for (int i = 0; i < length; i++) 
+		{
+			if (isFirstLetter)
+				text[i] = toUpperCase(text[i]);
+
+			isFirstLetter = (text[i] == ' ') ? true : false;
+		}
+
+		return text;
+	}
+
+	string firstLetterWordUpperCase()
+	{
+		return firstLetterWordUpperCase(_value);
+	}
+
+	static string firstLetterWordLowerCase(string text)
+	{
+		bool isFirstLetter = true;
+
+		short length = customLength(text);
+
+		for (int i = 0; i < length; i++)
+		{
+			if (isFirstLetter)
+				text[i] = toLowerCase(text[i]);
+
+			isFirstLetter = (text[i] == ' ') ? true : false;
+		}
+
+		return text;
+	}
+
+	string firstLetterWordLowerCase()
+	{
+		return firstLetterWordLowerCase(_value);
+	}
+
+
+	static string trimRight(string text) 
+	{
+		for (int i = 0; i < text.length(); i++) 
+		{
+			if (text[i] != ' ')
+				return mySubstr(text, i, text.length());
+		}
+
+		return text;
+	}
+
+	string trimRight() 
+	{
+		return trimRight(_value);
+	}
+
+	static string trimLeft(string text) 
+	{
+		for (int i = text.length() - 1; i >= 0; i--) 
+		{
+			if (text[i] != ' ')
+				return mySubstr(text, 0, i + 1);
+		}
+
+		return text;
+	}
+
+	string trimLeft() 
+	{
+		return trimLeft(_value);
+	}
+
+	static string trimAll(string text) 
+	{
+		text = trimRight(text);
+		text = trimLeft(text);
+
+		return text;
+	}
+
+	string trimAll() 
+	{
+		return trimAll(_value);
+	}
+
+	~MyString() 
+	{
+		cout << "The End Of String Object life Time\n";
+	}
+};
+
+
---
+++ Project1StringLibrary.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "MyString.h"
+using namespace std;
+
+int main() 
+{
+	MyString string1;
+
+	string1.value = "Mohammed Almislaty";
+
+	cout << "Name : " << string1.value << endl;
+
+	cout << "Number Of Words  : " << string1.countWords() << endl;
+
+	cout << "Number Of Words  : " << MyString::countWords("Mohammed Abdualrazag Salem Almislaty") << endl;
+
+	cout << "Upper Case Words   :    " << string1.toUpperCase() << endl;
+
+	cout << "Upper Case Words static function member   :    "
+		<< MyString::toUpperCase("Mohammed abdualrazag salem almislaty") << endl;
+
+	
+	cout << "Upper Case Words   :    " << string1.toLowerCase() << endl;
+
+	cout << "Upper Case Words static function member   :    "
+		<< MyString::toLowerCase("Mohammed Abdualrazag SalEm AlmIslaty") << endl;
+
+
+	cout << "Invert Case String   :    " << string1.invertString() << endl;
+
+	cout << "Invert Case String by static function member   :    "
+		<< MyString::invertString("Mohammed Abdualrazag SalEm AlmIslaty") << endl;
+
+	cout << "Substr string : " << string1.mySubstr(5, 13) << endl;
+	cout << "Substr string : " << string1.mySubstr(5, 18) << endl;
+
+	cout << "Custom length : " << string1.customLength("Mohammed Almislaty") << endl;
+
+	string koko = "Hello world";
+	cout << "Custom length : " << string1.customLength(koko) << endl;
+	cout << "Custom Length : " << string1.customLength() << endl;
+
+	cout << "Upper Case First Letter Of Every Word By static function member    : " 
+		<< string1.firstLetterWordUpperCase("mohammed abdualrazag salem almislaty") << endl;
+	cout << "Upper Case First Letter Of Every Word : " << string1.firstLetterWordUpperCase() << endl;
+
+
+	cout << "Lower Case First Letter Of Every Word By static function member    : "
+		<< string1.firstLetterWordLowerCase("Mohammed Abdualrazag Salem Almislaty") << endl;
+	cout << "Lower Case First Letter Of Every Word : " << string1.firstLetterWordLowerCase() << endl;
+
+	cout << "Substr String by static function member   : " 
+		<< MyString::mySubstr("Mohammed Almsialty", 8, 10) << endl;
+
+	//string test = "                                                      Mohammed Almislaty";
+
+	cout << "Trim Right via static function member : " 
+		<< MyString::trimRight("                      Mohammed Almislaty") << endl;
+	cout << "Trim Right  : " << string1.trimRight("                        Mohammed Almislaty") << endl;
+	cout << "Trim Right  : " << string1.trimRight() << endl;
+
+	cout << "Trim Left  via static function member : "
+		<< MyString::trimLeft("Mohammed Almislaty                ") << endl;
+	cout << "Trim Left   : " << string1.trimLeft() << endl;
+
+	cout << "Trim All    via static function member : " 
+		<< MyString::trimAll("                 MOhammed                ") << endl;
+	cout << "Trim All   : " << string1.trimAll() << endl;
+
+
+	system("pause > 0");
+
+
+	return 0;
+}
